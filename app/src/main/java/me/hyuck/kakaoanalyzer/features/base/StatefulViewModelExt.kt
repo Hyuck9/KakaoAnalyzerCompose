dcf5ab586689a09f80d79a3e306749f816bf4812
Hyuck <lhg1304@nexmore.co.kr>
2023-01-15 20:42:32 +0900

    o base ViewModel인 StatefulViewModel 생성

---
+++ app/src/main/java/me/hyuck/kakaoanalyzer/features/base/StatefulViewModelExt.kt
@@ -0,0 +1,21 @@
+package me.hyuck.kakaoanalyzer.features.base
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.coroutines.CoroutineScope
+
+@Composable
+fun <STATE, EFFECT, ACTION, ENVIRONMENT> HandleEffect(
+	viewModel: StatefulViewModel<STATE, EFFECT, ACTION, ENVIRONMENT>,
+	handle: suspend CoroutineScope.(EFFECT) -> Unit
+) {
+	val effect by viewModel.effect.collectAsStateWithLifecycle()
+	LaunchedEffect(effect) {
+		effect?.let {
+			handle(it)
+			viewModel.resetEffect()
+		}
+	}
+}
---
+++ app/src/main/java/me/hyuck/kakaoanalyzer/foundation/di/DispatcherModule.kt
@@ -0,0 +1,19 @@
+package me.hyuck.kakaoanalyzer.foundation.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DispatcherModule {
+
+	@Provides
+	@Singleton
+	fun provideDispatcherIO(): CoroutineDispatcher = Dispatchers.IO
+
+}
