ded52b2cb9730de5572d769666a6d9239189bc94
Hyuck <lhg1304@nexmore.co.kr>
2023-01-19 22:58:34 +0900

    o bottom menu ui 구성   -> 추후 테마 재정의해서 컬러셋 변경

--- app/src/main/java/me/hyuck/kakaoanalyzer/foundation/uicomponent/HomeBottomBar.kt
+++ app/src/main/java/me/hyuck/kakaoanalyzer/foundation/uicomponent/HomeBottomBar.kt
@@ -2,26 +2,45 @@ package me.hyuck.kakaoanalyzer.foundation.uicomponent
 
 import androidx.annotation.FloatRange
 import androidx.annotation.StringRes
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.SpringSpec
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.BoxScope
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.QuestionMark
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.*
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
+import androidx.core.os.ConfigurationCompat
 import me.hyuck.kakaoanalyzer.R
+import me.hyuck.kakaoanalyzer.foundation.theme.KakaoAnalyzerComposeTheme
+import java.util.*
 
 enum class HomeSections(
 	@StringRes val title: Int,
@@ -38,10 +57,169 @@ fun KakaoAnalyzerBottomBar(
 	tabs: Array<HomeSections>,
 	currentRoute: String,
 	navigateToRoute: (String) -> Unit,
-	color: Color = MaterialTheme.colorScheme.secondary,
-	contentColor: Color = MaterialTheme.colorScheme.primary
+	color: Color = MaterialTheme.colorScheme.surface,           // TODO: 추후 테마 재정의해서 컬러셋 변경
+	contentColor: Color = MaterialTheme.colorScheme.onSurface   // TODO: 추후 테마 재정의해서 컬러셋 변경
 ) {
+	val routes = remember { tabs.map { it.route } }
+	val currentSection = tabs.first { it.route == currentRoute }
 
+	KakaoAnalyzerSurface(
+		color = color,
+		contentColor = contentColor
+	) {
+		val springSpec = SpringSpec<Float>(
+			// Determined experimentally
+			stiffness = 800f,
+			dampingRatio = 0.8f
+		)
+		KakaoAnalyzerBottomNavLayout(
+			selectedIndex = currentSection.ordinal,
+			itemCount = routes.size,
+			indicator = { KakaoAnalyzerBottomNavIndicator() },
+			animSpec = springSpec,
+			modifier = Modifier.navigationBarsPadding()
+		) {
+			val configuration = LocalConfiguration.current
+			val currentLocale: Locale =
+				ConfigurationCompat.getLocales(configuration).get(0) ?: Locale.getDefault()
+
+			tabs.forEach { section ->
+				val selected = section == currentSection
+				val tint by animateColorAsState(
+					if (selected) {
+						MaterialTheme.colorScheme.primary     // TODO: 추후 테마 재정의해서 컬러셋 변경
+					} else {
+						MaterialTheme.colorScheme.primary     // TODO: 추후 테마 재정의해서 컬러셋 변경
+					}
+				)
+
+				val text = stringResource(section.title).uppercase(currentLocale)
+
+				KakaoAnalyzerBottomNavigationItem(
+					icon = {
+						Icon(
+							imageVector = section.icon,
+							tint = tint,
+							contentDescription = text
+						)
+					},
+					text = {
+						Text(
+							text = text,
+							color = tint,
+							style = androidx.compose.material.MaterialTheme.typography.button,
+							maxLines = 1
+						)
+					},
+					selected = selected,
+					onSelected = { navigateToRoute(section.route) },
+					animSpec = springSpec,
+					modifier = BottomNavigationItemPadding
+						.clip(BottomNavIndicatorShape)
+				)
+			}
+		}
+	}
+}
+
+@Composable
+private fun KakaoAnalyzerBottomNavLayout(
+	selectedIndex: Int,
+	itemCount: Int,
+	animSpec: AnimationSpec<Float>,
+	indicator: @Composable BoxScope.() -> Unit,
+	modifier: Modifier = Modifier,
+	content: @Composable () -> Unit
+) {
+	// Track how "selected" each item is [0, 1]
+	val selectionFractions = remember(itemCount) {
+		List(itemCount) { i ->
+			Animatable(if (i == selectedIndex) 1f else 0f)
+		}
+	}
+	selectionFractions.forEachIndexed { index, selectionFraction ->
+		val target = if (index == selectedIndex) 1f else 0f
+		LaunchedEffect(target, animSpec) {
+			selectionFraction.animateTo(target, animSpec)
+		}
+	}
+
+	// Animate the position of the indicator
+	val indicatorIndex = remember { Animatable(0f) }
+	val targetIndicatorIndex = selectedIndex.toFloat()
+	LaunchedEffect(targetIndicatorIndex) {
+		indicatorIndex.animateTo(targetIndicatorIndex, animSpec)
+	}
+
+	Layout(
+		modifier = modifier.height(BottomNavHeight),
+		content = {
+			content()
+			Box(Modifier.layoutId("indicator"), content = indicator)
+		}
+	) { measurables, constraints ->
+		check(itemCount == (measurables.size - 1)) // account for indicator
+
+		// Divide the width into n+1 slots and give the selected item 2 slots
+		val unselectedWidth = constraints.maxWidth / (itemCount + 1)
+		val selectedWidth = 2 * unselectedWidth
+		val indicatorMeasurable = measurables.first { it.layoutId == "indicator" }
+
+		val itemPlaceables = measurables
+			.filterNot { it == indicatorMeasurable }
+			.mapIndexed { index, measurable ->
+				// Animate item's width based upon the selection amount
+				val width = lerp(unselectedWidth, selectedWidth, selectionFractions[index].value)
+				measurable.measure(
+					constraints.copy(
+						minWidth = width,
+						maxWidth = width
+					)
+				)
+			}
+		val indicatorPlaceable = indicatorMeasurable.measure(
+			constraints.copy(
+				minWidth = selectedWidth,
+				maxWidth = selectedWidth
+			)
+		)
+
+		layout(
+			width = constraints.maxWidth,
+			height = itemPlaceables.maxByOrNull { it.height }?.height ?: 0
+		) {
+			val indicatorLeft = indicatorIndex.value * unselectedWidth
+			indicatorPlaceable.placeRelative(x = indicatorLeft.toInt(), y = 0)
+			var x = 0
+			itemPlaceables.forEach { placeable ->
+				placeable.placeRelative(x = x, y = 0)
+				x += placeable.width
+			}
+		}
+	}
+}
+
+@Composable
+fun KakaoAnalyzerBottomNavigationItem(
+	icon: @Composable BoxScope.() -> Unit,
+	text: @Composable BoxScope.() -> Unit,
+	selected: Boolean,
+	onSelected: () -> Unit,
+	animSpec: AnimationSpec<Float>,
+	modifier: Modifier = Modifier
+) {
+	Box(
+		modifier = modifier.selectable(selected = selected, onClick = onSelected),
+		contentAlignment = Alignment.Center
+	) {
+		// Animate the icon/text positions within the item based on selection
+		val animationProgress by animateFloatAsState(if (selected) 1f else 0f, animSpec)
+		KakaoAnalyzerBottomNavItemLayout(
+			icon = icon,
+			text = text,
+			animationProgress = animationProgress
+		)
+	}
 }
 
 @Composable
@@ -52,7 +230,25 @@ private fun KakaoAnalyzerBottomNavItemLayout(
 ) {
 	Layout(
 		content = {
-
+			Box(
+				modifier = Modifier
+					.layoutId("icon")
+					.padding(horizontal = TextIconSpacing),
+				content = icon
+			)
+			val scale = lerp(0.6f, 1f, animationProgress)
+			Box(
+				modifier = Modifier
+					.layoutId("text")
+					.padding(horizontal = TextIconSpacing)
+					.graphicsLayer {
+						alpha = animationProgress
+						scaleX = scale
+						scaleY = scale
+						transformOrigin = BottomNavLabelTransformOrigin
+					},
+				content = text
+			)
 		}
 	) {measurables, constraints ->
 		val iconPlaceable = measurables.first { it.layoutId == "icon" }.measure(constraints)
@@ -93,7 +289,7 @@ private fun MeasureScope.placeTextAndIcon(
 @Composable
 private fun KakaoAnalyzerBottomNavIndicator(
 	strokeWidth: Dp = 2.dp,
-	color: Color,
+	color: Color = MaterialTheme.colorScheme.onPrimary,     // TODO: 추후 테마 재정의해서 컬러셋 변경
 	shape: Shape = BottomNavIndicatorShape
 ) {
 	Spacer(
@@ -104,16 +300,21 @@ private fun KakaoAnalyzerBottomNavIndicator(
 	)
 }
 
+private val TextIconSpacing = 2.dp
+private val BottomNavHeight = 56.dp
+private val BottomNavLabelTransformOrigin = TransformOrigin(0f, 0.5f)
 private val BottomNavIndicatorShape = RoundedCornerShape(percent = 50)
 private val BottomNavigationItemPadding = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
 
 
-//@Preview
-//@Composable
-//private fun KakaoAnalyzerBottomNavItemLayoutPreview() {
-//	KakaoAnalyzerComposeTheme {
-//		KakaoAnalyzerBottomNavItemLayout(
-//
-//		)
-//	}
-//}
+@Preview
+@Composable
+private fun KakaoAnalyzerBottomNavPreview() {
+	KakaoAnalyzerComposeTheme {
+		KakaoAnalyzerBottomBar(
+			tabs = HomeSections.values(),
+			currentRoute = "home/chats",
+			navigateToRoute = { }
+		)
+	}
+}
