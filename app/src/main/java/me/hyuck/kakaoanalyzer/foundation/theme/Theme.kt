01b66efc27f48a1d00ffa27998f3cdfdd1123a50
Hyuck <lhg1304@nexmore.co.kr>
2023-01-21 16:48:01 +0900

    o Color Test

--- app/src/main/java/me/hyuck/kakaoanalyzer/foundation/theme/Theme.kt
+++ app/src/main/java/me/hyuck/kakaoanalyzer/foundation/theme/Theme.kt
@@ -16,15 +16,17 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
-	primary = Purple80,
-	secondary = PurpleGrey80,
-	tertiary = Pink80
+	primary = Shadow1,
+	secondary = Ocean2,
+	tertiary = Pink80,
+	background = Neutral8
 )
 
 private val LightColorScheme = lightColorScheme(
-	primary = Purple40,
-	secondary = PurpleGrey40,
-	tertiary = Pink40
+	primary = Shadow5,
+	secondary = Ocean3,
+	tertiary = Pink40,
+	background = Neutral0
 
 	/* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -41,7 +43,7 @@ private val LightColorScheme = lightColorScheme(
 fun KakaoAnalyzerComposeTheme(
 	darkTheme: Boolean = isSystemInDarkTheme(),
 	// Dynamic color is available on Android 12+
-	dynamicColor: Boolean = true,
+	dynamicColor: Boolean = false,
 	content: @Composable () -> Unit
 ) {
 	val colorScheme = when {
