b899eea7ccff6096c00975a4a089a7337600fe1f
Hyuck9 <lhg1304@gmail.com>
2023-02-04 19:33:43 +0900

    o Splash Test

--- app/src/main/java/me/hyuck/kakaoanalyzer/runtime/SplashActivity.kt
+++ app/src/main/java/me/hyuck/kakaoanalyzer/runtime/SplashActivity.kt
@@ -2,8 +2,10 @@ package me.hyuck.kakaoanalyzer.runtime
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -12,6 +14,11 @@ import kotlinx.coroutines.launch
 class SplashActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val splashScreen = installSplashScreen()
+            splashScreen.setKeepOnScreenCondition { true }
+        }
+
         super.onCreate(savedInstanceState)
 
         lifecycleScope.launch {
