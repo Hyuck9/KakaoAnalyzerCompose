63d086d75b5bf6ebc3dc6cd2948bae62c3bc31f1
Hyuck9 <lhg1304@gmail.com>
2023-03-09 13:20:33 +0900

    o Tab Layout 및 ViewPager 테스트 중

--- app/src/main/java/me/hyuck/kakaoanalyzer/features/statistics/common/ui/StatisticsScreen.kt
+++ app/src/main/java/me/hyuck/kakaoanalyzer/features/statistics/common/ui/StatisticsScreen.kt
@@ -1,12 +1,22 @@
 package me.hyuck.kakaoanalyzer.features.statistics.common.ui
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.coroutines.launch
 import me.hyuck.kakaoanalyzer.foundation.uicomponent.StatisticScaffold
 import me.hyuck.kakaoanalyzer.foundation.uicomponent.StatisticsBackHeader
 import me.hyuck.kakaoanalyzer.model.StatisticsTab
@@ -27,7 +37,10 @@ fun StatisticsScreen(
         }
     ) {
         ViewPagerWithTab(
-            tabs = state.statisticsTabs
+            modifier = Modifier.padding(it),
+            tabs = state.statisticsTabs,
+            selectedTab = state.selectedTab,
+            chatId = chatId
         )
     }
 }
@@ -36,11 +49,50 @@ fun StatisticsScreen(
 @Composable
 fun ViewPagerWithTab(
     modifier: Modifier = Modifier,
-    tabs: List<StatisticsTab>
+    tabs: List<StatisticsTab>,
+    selectedTab: StatisticsTab,
+    chatId: String
 ) {
+    val selectedIndex = tabs.indexOfFirst { it == selectedTab }
     val pagerState = rememberPagerState()
     val coroutineScope = rememberCoroutineScope()
 
-
+    Column(
+        modifier = modifier,
+    ) {
+        TabRow(
+            selectedTabIndex = selectedIndex,
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    Modifier.tabIndicatorOffset(tabPositions[selectedIndex])
+                )
+            }
+        ) {
+            tabs.forEachIndexed { index, statisticsTab ->
+                Tab(
+                    selected = index == selectedIndex,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                    text = {
+                        Text(
+                            text = statisticsTab.value
+                        )
+                    }
+                )
+            }
+        }
+        HorizontalPager(
+            modifier = Modifier.fillMaxSize(),
+            pageCount = tabs.count(),
+            state = pagerState
+        ) { page ->
+            Text(
+                text = "Statistics[${tabs[page].value}]  - $chatId"
+            )
+        }
+    }
 
 }
