736e4b4540096ef5b51aa472d6183eea9b836fcf
Hyuck <lhg1304@nexmore.co.kr>
2023-03-11 17:50:05 +0900

    o SelectorButton Component 추가

--- app/src/main/java/me/hyuck/kakaoanalyzer/foundation/uicomponent/Button.kt
+++ app/src/main/java/me/hyuck/kakaoanalyzer/foundation/uicomponent/Button.kt
@@ -1,14 +1,17 @@
 package me.hyuck.kakaoanalyzer.foundation.uicomponent
 
-import androidx.compose.material.IconButton
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
-import androidx.compose.material3.Icon
-import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun KakaoAnalyzerIconButton(
@@ -25,6 +28,38 @@ fun KakaoAnalyzerIconButton(
             imageVector = imageVector,
             tint = tint,
             contentDescription = ""
+        )
+    }
+}
+
+@Composable
+fun SelectorButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    onClick: () -> Unit
+) {
+    OutlinedButton(
+        modifier = modifier.height(32.dp),
+        onClick = onClick,
+        shape = RoundedCornerShape(2.dp)
+    ) {
+        Text(
+            text = text,
+            fontSize = 12.sp,
+            color = Color.Black
+        )
+    }
+}
+
+
+
+@Preview
+@Composable
+private fun SelectorButtonPreview() {
+    Surface {
+        SelectorButton(
+            text = "2023-03-11",
+            onClick = {}
         )
     }
 }
