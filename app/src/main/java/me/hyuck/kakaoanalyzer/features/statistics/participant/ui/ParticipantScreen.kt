a73447dde209e77f254fb2807f34a4f16ccd74cb
Hyuck <lhg1304@nexmore.co.kr>
2023-03-18 04:44:20 +0900

    o 사용자 분석 item 임의로 표출

--- app/src/main/java/me/hyuck/kakaoanalyzer/features/statistics/participant/ui/ParticipantScreen.kt
+++ app/src/main/java/me/hyuck/kakaoanalyzer/features/statistics/participant/ui/ParticipantScreen.kt
@@ -1,11 +1,19 @@
 package me.hyuck.kakaoanalyzer.features.statistics.participant.ui
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import me.hyuck.kakaoanalyzer.foundation.uicomponent.ParticipantItemCell
 import me.hyuck.kakaoanalyzer.model.Chat
+import me.hyuck.kakaoanalyzer.model.Participant
 
 @Composable
 fun ParticipantScreen(
@@ -15,7 +23,24 @@ fun ParticipantScreen(
 	viewModel.initChat(chat)
 	val state by viewModel.state.collectAsStateWithLifecycle()
 
-	Text(
-		text = "ParticipantScreen  - ${chat.id}"
-	)
+	ChatsContent(participants = state.items)
+}
+
+@Composable
+private fun ChatsContent(
+	modifier: Modifier = Modifier,
+	participants: List<Participant>,
+) {
+	LazyColumn(
+		modifier = modifier
+			.fillMaxSize()
+			.padding(all = 16.dp)
+	) {
+		items(
+			items = participants,
+			key = { item -> item.userName }
+		) { participant ->
+			ParticipantItemCell(participant = participant)
+		}
+	}
 }
