9694ac26f93c876c6735961a7d28f5bf68574f88
Hyuck <lhg1304@nexmore.co.kr>
2023-03-19 22:33:17 +0900

    o 함수명 변경

--- app/src/main/java/me/hyuck/kakaoanalyzer/features/statistics/participant/ui/ParticipantScreen.kt
+++ app/src/main/java/me/hyuck/kakaoanalyzer/features/statistics/participant/ui/ParticipantScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -23,11 +22,11 @@ fun ParticipantScreen(
 	viewModel.initChat(chat)
 	val state by viewModel.state.collectAsStateWithLifecycle()
 
-	ChatsContent(participants = state.items)
+	ParticipantContent(participants = state.items)
 }
 
 @Composable
-private fun ChatsContent(
+private fun ParticipantContent(
 	modifier: Modifier = Modifier,
 	participants: List<Participant>,
 ) {
