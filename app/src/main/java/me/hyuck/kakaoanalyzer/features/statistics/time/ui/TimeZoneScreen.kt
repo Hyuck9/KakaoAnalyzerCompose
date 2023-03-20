f47a551b42a452bbd08bff2744d1dd6da2343ed8
Hyuck9 <lhg1304@gmail.com>
2023-03-20 20:58:41 +0900

    o MPAndroidChart 테스트중   -> notifyDataSetChanged 하지 않으면 State 변경, 리컴포저블 발생시 자동으로 변경되지 않기 때문에 데이터 셋팅 방법 변경 또는 라이브러리 변경 필요

--- app/src/main/java/me/hyuck/kakaoanalyzer/features/statistics/time/ui/TimeZoneScreen.kt
+++ app/src/main/java/me/hyuck/kakaoanalyzer/features/statistics/time/ui/TimeZoneScreen.kt
@@ -1,10 +1,26 @@
 package me.hyuck.kakaoanalyzer.features.statistics.time.ui
 
+import android.graphics.Color
+import android.graphics.DashPathEffect
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import me.hyuck.kakaoanalyzer.R
 import me.hyuck.kakaoanalyzer.model.Chat
+import me.hyuck.kakaoanalyzer.model.TimeZone
+import splitties.resources.appStr
+import kotlin.streams.toList
 
 @Composable
 fun TimeZoneScreen(
@@ -14,4 +30,66 @@ fun TimeZoneScreen(
     viewModel.initChat(chat)
     val state by viewModel.state.collectAsStateWithLifecycle()
 
+    TimeZoneContent(times = state.items)
+}
+
+@Composable
+private fun TimeZoneContent(
+    modifier: Modifier = Modifier,
+    times: List<TimeZone>
+) {
+    AndroidView(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        factory = { context ->
+            val lineChart = LineChart(context).apply {
+                description.isEnabled = false
+                setTouchEnabled(true)
+                setDrawGridBackground(false)
+                isDragEnabled = true
+                setScaleEnabled(true)
+                setPinchZoom(true)
+                animateX(1500)
+                legend.form = Legend.LegendForm.LINE
+                data = getLineData(times)
+                data.notifyDataChanged()
+                invalidate()
+                notifyDataSetChanged()
+            }
+            lineChart
+        }
+    )
+}
+
+private fun getLineData(times: List<TimeZone>): LineData {
+    val entries = times.stream()
+        .map {
+            Entry(it.hour.toFloat(), it.messageCount.toFloat())
+        }.toList()
+
+    val dataSet = LineDataSet(entries, appStr(R.string.label_chart_timezone)).apply {
+        setDrawIcons(false)
+        enableDashedLine(10f, 5f, 0f)
+
+        color = Color.BLACK
+        setCircleColor(Color.BLACK)
+
+        lineWidth = 1f
+        circleRadius = 3f
+
+        setDrawCircleHole(false)
+
+        formLineWidth = 1f
+        formLineDashEffect = DashPathEffect(floatArrayOf(10f, 5f), 0f)
+        formSize = 15f
+
+        valueTextSize = 9f
+
+        enableDashedHighlightLine(10f, 5f, 0f)
+
+        setDrawFilled(true)
+    }
+
+    return LineData(dataSet)
 }
