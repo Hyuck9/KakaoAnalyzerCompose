719ed963ba16a9261b0070482922b2901b7175e2
Hyuck9 <lhg1304@gmail.com>
2023-03-21 19:36:18 +0900

    o 라인차트 gradient 추가

--- app/src/main/java/me/hyuck/kakaoanalyzer/features/statistics/time/ui/TimeZoneScreen.kt
+++ app/src/main/java/me/hyuck/kakaoanalyzer/features/statistics/time/ui/TimeZoneScreen.kt
@@ -1,7 +1,5 @@
 package me.hyuck.kakaoanalyzer.features.statistics.time.ui
 
-import android.graphics.Color
-import android.graphics.DashPathEffect
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
@@ -16,14 +14,9 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.Legend
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
-import me.hyuck.kakaoanalyzer.R
 import me.hyuck.kakaoanalyzer.model.Chat
 import me.hyuck.kakaoanalyzer.model.TimeZone
-import splitties.resources.appStr
-import kotlin.streams.toList
+import me.hyuck.kakaoanalyzer.model.getLineData
 
 @Composable
 fun TimeZoneScreen(
@@ -56,7 +49,7 @@ private fun TimeZoneContent(
     }
 
     LaunchedEffect(times) {
-        lineChart.data = getLineData(times)
+        lineChart.data = times.getLineData()
         lineChart.data.notifyDataChanged()
         lineChart.invalidate()
         lineChart.notifyDataSetChanged()
@@ -70,36 +63,4 @@ private fun TimeZoneContent(
             lineChart
         }
     )
-}
-
-private fun getLineData(times: List<TimeZone>): LineData {
-    val entries = times.stream()
-        .map {
-            Entry(it.hour.toFloat(), it.messageCount.toFloat())
-        }.toList()
-
-    val dataSet = LineDataSet(entries, appStr(R.string.label_chart_timezone)).apply {
-        setDrawIcons(false)
-        enableDashedLine(10f, 5f, 0f)
-
-        color = Color.BLACK
-        setCircleColor(Color.BLACK)
-
-        lineWidth = 1f
-        circleRadius = 3f
-
-        setDrawCircleHole(false)
-
-        formLineWidth = 1f
-        formLineDashEffect = DashPathEffect(floatArrayOf(10f, 5f), 0f)
-        formSize = 15f
-
-        valueTextSize = 9f
-
-        enableDashedHighlightLine(10f, 5f, 0f)
-
-        setDrawFilled(true)
-    }
-
-    return LineData(dataSet)
 }
--- app/src/main/java/me/hyuck/kakaoanalyzer/model/TimeZone.kt
+++ app/src/main/java/me/hyuck/kakaoanalyzer/model/TimeZone.kt
@@ -1,6 +1,45 @@
 package me.hyuck.kakaoanalyzer.model
 
+import android.graphics.Color
+import android.graphics.DashPathEffect
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import me.hyuck.kakaoanalyzer.R
+import splitties.resources.appDrawable
+import splitties.resources.appStr
+
 data class TimeZone(
     val hour: String,
     val messageCount: Int
 )
+
+fun List<TimeZone>.getLineData(): LineData {
+    val entries = this.map { Entry(it.hour.toFloat(), it.messageCount.toFloat()) }
+
+    val dataSet = LineDataSet(entries, appStr(R.string.label_chart_timezone)).apply {
+        setDrawIcons(false)
+        enableDashedLine(10f, 5f, 0f)
+
+        color = Color.BLACK
+        setCircleColor(Color.BLACK)
+
+        lineWidth = 1f
+        circleRadius = 3f
+
+        setDrawCircleHole(false)
+
+        formLineWidth = 1f
+        formLineDashEffect = DashPathEffect(floatArrayOf(10f, 5f), 0f)
+        formSize = 15f
+
+        valueTextSize = 9f
+
+        enableDashedHighlightLine(10f, 5f, 0f)
+
+        setDrawFilled(true)
+        fillDrawable = appDrawable(R.drawable.shape_vertical_gradient)
+    }
+
+    return LineData(dataSet)
+}
