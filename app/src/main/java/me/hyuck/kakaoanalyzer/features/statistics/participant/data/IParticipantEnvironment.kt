233a6e76dcddaea14dd9425b20416315bb70d102
Hyuck <lhg1304@nexmore.co.kr>
2023-04-02 03:29:29 +0900

    o 불필요 코드 제거

--- app/src/main/java/me/hyuck/kakaoanalyzer/features/statistics/participant/data/IParticipantEnvironment.kt
+++ app/src/main/java/me/hyuck/kakaoanalyzer/features/statistics/participant/data/IParticipantEnvironment.kt
@@ -3,7 +3,6 @@ package me.hyuck.kakaoanalyzer.features.statistics.participant.data
 import kotlinx.coroutines.flow.Flow
 import me.hyuck.kakaoanalyzer.model.Chat
 import me.hyuck.kakaoanalyzer.model.Message
-import me.hyuck.kakaoanalyzer.model.OneOnOneAnalyticsInfo
 import me.hyuck.kakaoanalyzer.model.Participant
 
 interface IParticipantEnvironment {
@@ -11,7 +10,5 @@ interface IParticipantEnvironment {
 	fun getParticipants(chat: Chat, limit: Int = -1): Flow<List<Participant>>
 
 	fun getMessages(chat: Chat, limit: Int = -1): Flow<List<Message>>
-
-	fun analysisMessages(messages: List<Message>): OneOnOneAnalyticsInfo
 
 }
--- app/src/main/java/me/hyuck/kakaoanalyzer/features/statistics/participant/data/ParticipantEnvironment.kt
+++ app/src/main/java/me/hyuck/kakaoanalyzer/features/statistics/participant/data/ParticipantEnvironment.kt
@@ -4,7 +4,6 @@ import kotlinx.coroutines.flow.Flow
 import me.hyuck.kakaoanalyzer.foundation.data.repository.message.MessageRepository
 import me.hyuck.kakaoanalyzer.model.Chat
 import me.hyuck.kakaoanalyzer.model.Message
-import me.hyuck.kakaoanalyzer.model.OneOnOneAnalyticsInfo
 import me.hyuck.kakaoanalyzer.model.Participant
 import javax.inject.Inject
 
@@ -15,13 +14,5 @@ class ParticipantEnvironment @Inject constructor(
 	override fun getParticipants(chat: Chat, limit: Int): Flow<List<Participant>> = messageRepository.getParticipants(chat, limit)
 
 	override fun getMessages(chat: Chat, limit: Int): Flow<List<Message>> = messageRepository.getMessages(chat, limit)
-
-	override fun analysisMessages(messages: List<Message>): OneOnOneAnalyticsInfo {
-		return OneOnOneAnalyticsInfo(allMessages = messages)
-	}
-
-	private fun test() {
-
-	}
 
 }
