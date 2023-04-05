c8a50566a140ef3d1499b5930e4ea8a263c1641b
Hyuck9 <lhg1304@gmail.com>
2023-04-05 20:24:37 +0900

    o detail 화면에 키워드 검색창 추가

--- app/src/main/java/me/hyuck/kakaoanalyzer/features/detail/ui/DetailScreen.kt
+++ app/src/main/java/me/hyuck/kakaoanalyzer/features/detail/ui/DetailScreen.kt
@@ -1,33 +1,29 @@
 package me.hyuck.kakaoanalyzer.features.detail.ui
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Clear
-import androidx.compose.material.icons.outlined.Search
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import me.hyuck.kakaoanalyzer.R
 import me.hyuck.kakaoanalyzer.foundation.extension.testLog
-import me.hyuck.kakaoanalyzer.foundation.theme.KakaoAnalyzerTheme
-import me.hyuck.kakaoanalyzer.foundation.uicomponent.KakaoAnalyzerSurface
+import me.hyuck.kakaoanalyzer.foundation.uicomponent.ContentRow
+import me.hyuck.kakaoanalyzer.foundation.uicomponent.SearchBar
 import me.hyuck.kakaoanalyzer.foundation.uicomponent.StatisticScaffold
 import me.hyuck.kakaoanalyzer.foundation.uicomponent.StatisticsBackHeader
 import me.hyuck.kakaoanalyzer.model.DetailType
+import me.hyuck.kakaoanalyzer.model.Keyword
 
 @Composable
 fun DetailScreen(
 	viewModel: DetailViewModel = hiltViewModel(),
-	detailType: DetailType,
+	detailType: DetailType = DetailType.KEYWORD,
 	upPress: () -> Unit
 ) {
 
@@ -46,80 +42,32 @@ fun DetailScreen(
 			}
 		}
 	) {
-
-	}
-}
-
-@Composable
-private fun SearchBar(
-	modifier: Modifier = Modifier,
-	query: TextFieldValue = TextFieldValue(""),
-	onQueryChange: (TextFieldValue) -> Unit = {},
-	onClearQuery: () -> Unit = {}
-) {
-	KakaoAnalyzerSurface(
-		shape = MaterialTheme.shapes.small,
-		modifier = modifier
-			.fillMaxWidth()
-			.height(56.dp)
-			.padding(horizontal = 24.dp, vertical = 8.dp)
-	) {
-		Box(Modifier.fillMaxSize()) {
-			if (query.text.isEmpty()) {
-				SearchHint()
-			}
-			Row(
-				verticalAlignment = Alignment.CenterVertically,
-				modifier = Modifier
-					.fillMaxSize()
-					.wrapContentHeight()
-			) {
-				BasicTextField(
-					value = query,
-					onValueChange = onQueryChange,
-					modifier = Modifier
-						.weight(1f)
-				)
-				if (query.text.isNotEmpty()) {
-					IconButton(onClick = onClearQuery) {
-						Icon(
-							imageVector = Icons.Outlined.Clear,
-							contentDescription = stringResource(id = R.string.label_clear)
-						)
-					}
-				}
-			}
+		Column(
+			modifier = Modifier
+				.fillMaxSize()
+				.padding(it)
+		) {
+			SearchBar()
+			DetailContent()
 		}
 	}
 }
 
 @Composable
-private fun SearchHint() {
-	Row(
-		verticalAlignment = Alignment.CenterVertically,
-		modifier = Modifier
+fun DetailContent(
+	modifier: Modifier = Modifier,
+	keywords: List<Keyword> = emptyList(),
+) {
+	LazyColumn(
+		modifier = modifier
 			.fillMaxSize()
-			.wrapContentSize()
+			.padding(horizontal = 16.dp)
 	) {
-		Icon(
-			imageVector = Icons.Outlined.Search,
-			contentDescription = stringResource(id = R.string.label_search)
-		)
-		Spacer(modifier = Modifier.width(8.dp))
-		Text(
-			text = stringResource(id = R.string.search_keyword)
-		)
-	}
-}
-
-
-@Preview
-@Composable
-private fun SearchBarPreview() {
-	KakaoAnalyzerTheme {
-		KakaoAnalyzerSurface {
-			SearchBar(
-			)
+		items(
+			items = keywords,
+			key = { item -> item.word }
+		) { keyword ->
+			ContentRow(title = keyword.word, count = keyword.wordCount)
 		}
 	}
 }
